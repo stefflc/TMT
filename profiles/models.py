c7d63e825d5ee9d7c9da3002a60a0b6204119bbf
Chad <cpusteffl@hotmail.com>
2025-04-01 14:48:18 -0600

    ## challenge-4-userprofile-and-admin - add `UserProfile` - add admin for Inventory, Order - forgot to add some files

---
+++ profiles/models.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field is required.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+# This is painful to write, let's instead make Profile or UserProfile and make it OneToOne with django.auth.User class so we don't have to reinvent the wheel.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Inherit from AbstractBaseUser
+    password, last_login
+    PermissionsMixin grants is_superuser field
+    """
+    # forgot how to do this offhand, need to hash it.. something salt
+
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    email = models.EmailField(unique=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def get_username(self):
+        return self.email  # since email is used as username
+
+    @property
+    def is_authenticated(self):
+        # I believe Django normally just does this automatically
+        return True
