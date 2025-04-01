0ecc87b8fa4a720705cfb4bd3fd2b0a4fea7e920
Chad <cpusteffl@hotmail.com>
2025-04-01 15:00:10 -0600

    ## challenge-7-orders-by-tagid

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,9 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
-
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrdersByTagView
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path("tags/<int:tag_id>/orders/", OrdersByTagView.as_view(), name="orders-by-tag"),
 ]
