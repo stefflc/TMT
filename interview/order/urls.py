976aa3f452a611b9fcf04b3183c745204e947409
Chad <cpusteffl@hotmail.com>
2025-04-01 14:23:59 -0600

    ## challenge-3-startdate_embargo_date - add view `OrdersWithinDateRangeView` at endpoint /orders/date-range/ *  requires both `created_date` and `embargo_date` query param in YYYY-MM-DD format

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,9 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
-
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrdersWithinDateRangeView
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path("date-range/", OrdersWithinDateRangeView.as_view(), name="orders-by-date-range"),
 ]
